f56e1eb11a967f4af099e210adbbd914947ea298
trpaleho75 <travisholt@hotmail.com>
2019-01-22 13:52:31 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] myArray = new int[] {5, 7, 3, 9, 12, 15};
+        MergeSort mySortedArray = new MergeSort(myArray);
+        mySortedArray.toString();
+    }
+
+}
